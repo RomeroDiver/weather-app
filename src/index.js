0b12d194f834cddeb73601dab55c251d194fd335
Roman Pylypets <roman.pylypets@hotmail.com>
2018-07-31 22:58:55 +0200

    Add basic styles and mainApp component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import WeatherCard from './WeatherCard';
+import WeatherStore, { WeatherContext } from './WeatherStore';
+import Search from './Search';
+import './styles.css';
 
+/**
+ * TODO:
+ * 1. UI changes
+ * 2. Weather history
+ * 3. Next/previous day
+ */
 const Index = () => {
-  return <div>Hello React!</div>;
+  return (
+    <WeatherStore>
+      <WeatherContext.Consumer>
+        {
+          ({ weather, actions }) => {
+            return (
+              <main className="mainApp">
+                <div className="mainApp__header">
+                  <Search loadWeather={actions.loadWeather} />
+                </div>
+                <section className="mainApp__content">
+                  { weather && <WeatherCard weather={weather} /> }
+                </section>
+              </main>
+            );
+          }
+        }
+      </WeatherContext.Consumer>
+    </WeatherStore>
+  );
 };
 
 ReactDOM.render(<Index />, document.getElementById("app"));
