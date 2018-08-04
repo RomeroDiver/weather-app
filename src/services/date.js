5b5b4d31cfb2b976f3f709b43612c71f5a8aacbb
Roman Pylypets <roman.pylypets@hotmail.com>
2018-08-04 11:59:38 +0200

    Add format hour helper

--- src/services/date.js
+++ src/services/date.js
@@ -9,3 +9,24 @@ const days = [
 ];
 
 export const getDay = dayIndex => days[dayIndex];
+
+/**
+ * @param {Date} date
+ */
+export const formatHour = (date) => {
+  const minutes = date.getMinutes();
+  let hour = date.getHours();
+  const amPM = hour > 11 ? 'PM' : 'AM';
+  if (hour > 12) {
+    hour -= 12;
+  } else if (hour == 0) {
+    hour = '12';
+  }
+  return addZero`${hour}:${minutes}${amPM}`;
+}
+
+function addZero(raw, hours, minutes, amPm) {
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedHour = hours < 10 ? `0${hours}` : hours;
+  return `${formattedHour}:${formattedMinutes}${amPm}`;
+}
